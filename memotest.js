c5ed656d2a5b6ab55861b6ee77312c248b84ab1e
Cristian <cristianvillar252@gmail.com>
2023-08-17 18:32:44 -0300

    Creates selectRandomSquares( ) function
    This will return an array compound by a random selection of squares saved in "randomSquares" variable.

--- memotest.js
+++ memotest.js
@@ -3,6 +3,9 @@ const $status = document.querySelector("#status");
 const $startbutton = document.querySelector("#start-button");
 
 const $squares = document.querySelectorAll(".square");
+function selectRandomSquares() {
+  return randomSquares;
+}
 function generateRandomNumber(max) {
   const randomNumber = Math.floor(Math.random() * max);
 
