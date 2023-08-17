b00793112d8fca0205af78e5d909203156b642f6
Cristian <cristianvillar252@gmail.com>
2023-08-17 17:08:56 -0300

    Creates $squares
    For contain the squares of the memotest.

--- memotest.js
+++ memotest.js
@@ -1,3 +1,5 @@
 const $remainingCoincidences = document.querySelector("#coincidences");
 const $status = document.querySelector("#status");
 const $startbutton = document.querySelector("#start-button");
+
+const $squares = document.querySelectorAll(".square");
